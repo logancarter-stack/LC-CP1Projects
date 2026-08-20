bcdeb23d4299c95af55462357ec8bda8977ea608
logancarter-stack <logan.carter@ucas-edu.net>
2026-08-20 09:42:06 -0600

    added code

---
+++ character_introduction.py
@@ -0,0 +1,10 @@
+#logan carter character introduction
+name=input("what is your name: ")
+
+age=input("how old are you: ")
+
+job=input("what is your job title: ")
+
+hometown=input("where are you from: ")
+
+print("Hi, my name is "+ name + ". I'm " + age + " years old., I work as a " + job + ", and I'm from " + hometown + ".")
