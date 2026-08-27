7e6eea07fc66cbc142bc3be590c059fc8a1d27d3
logancarter-stack <logan.carter@ucas-edu.net>
2026-08-27 09:34:00 -0600

    changed psudocode and added my code

--- unit1_final_code.py
+++ unit1_final_code.py
@@ -1,13 +1,7 @@
 # logan carter, Unit 1 final Project
 # ask user what their name is, what their favorite color is, what their favorite animal, is and their age is.( in that order)
-# first print hello + name
-# then print so let me recap your name is + name + your are + age + years old and you like + color + animal + s.
+# first print hello + name# then print so let me recap your name is + name + your are + age + years old and you like the color + color + and +animal + s.
 # then ask the user if its corect 
-# if yes than ok I did it
-# if no than oh your name is + name + you are + age + years old and you like the color + color + and your favorite animal is a + animal + .
-# then ask the user if its corect again
-# if yes than ok I did it 
-# if no than ok I quit
 
 #variables 
 # name = input("What is you name:)
@@ -16,3 +10,12 @@
 # animal = input("What is you favorite animal:")
 
 
+name = input("What is your name: ")
+print("Hello " + name + "." )
+age = input("How old are you: ")
+print("You're "+ age +" wow!")
+color = input("What's you favorite color:")
+print("Wow " + color + " that is the best color.")
+animal = input("What is you favorite animal:")
+print( animal + "'s are my favorite too.")
+print("So lets recap your name is " + name + ". You are " + age + " years old, and you like the color " + color + " and " + animal + "'s.")
