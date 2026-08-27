679ba18f0f403e8005ce10e61657f743d660df4c
logancarter-stack <logan.carter@ucas-edu.net>
2026-08-27 09:13:50 -0600

    added psudecode

---
+++ unit1_final_code.py
@@ -0,0 +1,18 @@
+# logan carter, Unit 1 final Project
+# ask user what their name is, what their favorite color is, what their favorite animal, is and their age is.( in that order)
+# first print hello + name
+# then print so let me recap your name is + name + your are + age + years old and you like + color + animal + s.
+# then ask the user if its corect 
+# if yes than ok I did it
+# if no than oh your name is + name + you are + age + years old and you like the color + color + and your favorite animal is a + animal + .
+# then ask the user if its corect again
+# if yes than ok I did it 
+# if no than ok I quit
+
+#variables 
+# name = input("What is you name:)
+# age = input("How old are you:)
+# color = input("What's you favorite color:")
+# animal = input("What is you favorite animal:")
+
+
