c106acc659385baf54e7fcc559268953c4b88ff5
logancarter-stack <logan.carter@ucas-edu.net>
2026-09-02 09:53:28 -0600

    fixed problesms

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,56 +1,52 @@
-
-while True:
-    try:
-        one=int(input("what is your grade for first period:"))
-    except:
-        print("thats not a number")    
+dumb dumb")    
     else:
         break
-
+two=input("what is your grade for second period:")
 while True:
     try:
         two=int(input("what is your grade for second period:"))
     except:
-        print("thats not a number")    
+        print("thats not a number dumb dumb")    
     else:
         break
-
+three=input("what is your grade for third period:")
 while True:
     try:
         three=int(input("what is your grade for third period:"))
     except:
-        print("thats not a number")    
+        print("thats not a number dumb dumb")    
     else:
         break
+four=input("what is your grade for fourth period:")
 while True:
     try:
         four=int(input("what is your grade for fourth period:"))
     except:
-        print("thats not a number")    
+        print("thats not a number dumb dumb")    
     else:
         break
+five=input("what is your grade for fifth period:")
 while True:
     try:
         five=int(input("what is your grade for fifth period:"))
     except:
-        print("thats not a number")    
+        print("thats not a number dumb dumb")    
     else:
         break
+six=input("what is your grade for sixth period:")
 while True:
     try:
         six=int(input("what is your grade for sixth period:"))
     except:
-        print("thats not a number")    
+        print("thats not a number dumb dumb")    
     else:
         break
-
+seven=input("what is your grade for seventh period:")
 while True:
     try:
         seven=int(input("what is your grade for seventh period:"))
     except:     
-        print("thats not a number")    
+        print("thats not a number dumb dumb")    
     else:
         break
-total_num=one+two+three+four+five+six+seven
-average_grade=(total_num/7)
-print(average_grade)
+total_n
