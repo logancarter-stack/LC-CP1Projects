d43649a74694eb61dc7489138786d114ad038f66
Gustavo the pole <logan.carter@ucas-edu.net>
2026-09-08 22:37:13 +0000

    added more code

--- idiot_proof.py
+++ idiot_proof.py
@@ -15,3 +15,11 @@ while True:
 print(f"calling {phone}")
 print("Ring ring ring Ring ring ring Ring ring ring")
 print(f"*over phone* hello is this {name} ?")
+while True:
+    try 
+        gpa=int(input("what is your gpa"))
+    except:
+        print("hey stupid that\'s not a number.")
+    else:
+        break  
+        
