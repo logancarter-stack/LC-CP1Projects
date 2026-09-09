d1e5d035040e5820ee600d6a957f451e7d3b50e1
logancarter-stack <logan.carter@ucas-edu.net>
2026-09-09 09:43:33 -0600

    don't trust float

--- using_random.py
+++ using_random.py
@@ -1,7 +1,14 @@
 import random
 
-ducks=random.randint(1,10)
+ ducks=random.randint(1,10)
 
 print(f'there are {ducks} ducks')
 print(f"bang bang bang")
 print(f"now there are no ducks")
+
+
+pens=random.randrange(0,18,3)
+print(f"me has {pens} pens") 
+
+grade=random.random()
+print(f'your grade is {grade:.2}')
