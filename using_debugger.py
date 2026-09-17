ff11e2061f94b9d623e1aecbe3275796ad098281
logancarter-stack <logan.carter@ucas-edu.net>
2026-09-17 09:53:11 -0600

    debugged

--- using_debugger.py
+++ using_debugger.py
@@ -1,21 +1,21 @@
-# Ravager Snack Bar
+# LC 1 Ravager Snack Bar 
 import random
 
 pirate_name = input("What's your name, pirate? ")
 snack_name = input("What snack do you want? ")
 
 price = random.randint(2, 8)  # random price in credits
-quantity = input("How many would you like? ")
+quantity = int(input("How many would you like? "))
 
 total = price * quantity
-
-discounted_total = total - 2 * 0.10
+total_new = int(total)#made total an interger
+discounted_total = total_new -2 * 0.10
 
 tax_rate = 0.08
 total_with_tax = discounted_total + (discounted_total * tax_rate)
 
 print("Hello, " + pirate_name + "! Here's your order summary:")
-print("Snack: " + snack_name)#syntax error fixed :
+print("Snack: " + snack_name)#runtime error fixed : change snackName to snack_name
 print("Price per snack: " + str(price) + " credits")
-print("Total before tax: " + str(price))
+print("Total before tax: "  + str(discounted_total))# printed the same thing as above now it print what is was meant to 
 print("Total with tax: " + str(round(total_with_tax, 2)) + " credits")# syntax error fixed : no parenthesse to parenthesses
